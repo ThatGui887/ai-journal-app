80384c79505c7f8d1a22ba6955d030aa82665d3e
ThatGui887 <markopienaar2@gmail.com>
2026-03-06 16:27:06 +0200

    exported our schema to be used. added entries for our DB

--- convex/_generated/DB_schema.ts
+++ convex/_generated/DB_schema.ts
@@ -1,2 +1,11 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
+
+export default defineSchema({
+    entries: defineTable({
+        text: v.string(),
+        summary: v.string(),
+        mood: v.string(),
+        createdAt: v.number(),
+    }),
+});
